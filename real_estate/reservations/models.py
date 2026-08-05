ad0de21a844078ab67645a81fb5b8b1138233f98
Student <Student@top.ru>
2026-08-05 20:24:38 +0300

    Practice 1

--- real_estate/reservations/models.py
+++ real_estate/reservations/models.py
@@ -4,4 +4,14 @@ class Places(models.Model):
     address = models.CharField(max_length=255)
     price = models.IntegerField()
     
+# Практика 1
+#  Добавьте в класс Places поле built_at
+#  типа DateField с датой сдачи 
+#  в эксплуатацию здания / квартиры
+#  и поле rooms (IntegerField) с 
+#  количеством комнат
+#  Примените изменения к базе данных
+#  (makemigrations + migrate)
+
+    
 
