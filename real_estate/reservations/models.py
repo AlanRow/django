72240c9e05d2d72af0600c35f01a7d5f59aa5b8f
Student <Student@top.ru>
2026-08-05 20:57:46 +0300

    add fields built_at & rooms

--- real_estate/reservations/models.py
+++ real_estate/reservations/models.py
@@ -1,8 +1,12 @@
+from datetime import date
 from django.db import models
 
 class Places(models.Model):
     address = models.CharField(max_length=255)
-    price = models.IntegerField()
+    price = models.FloatField()
+    built_at = models.DateField(default=date.today)
+    rooms = models.IntegerField()
+    
     
 # Практика 1
 #  Добавьте в класс Places поле built_at
