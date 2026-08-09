59bc47be5e18e163db07456084316d174f5e4062
a.rogatskin <a.rogatskin@sitesoft.ru>
2026-08-09 23:12:03 +0300

    add templates

--- reservations/urls.py
+++ reservations/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("place/<str:id>", views.place_by_id, name="place_by_id"),
+    path("places", views.places_list, name="places")
 ]
--- reservations/views.py
+++ reservations/views.py
@@ -1,6 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
 
-# Create your views here.
+from .models import Place
 
 def index(request):
     return HttpResponse("Hello, World!")
+
+def place_by_id(request, id):
+    try:
+        place = Place.objects.get(pk=id)
+    except Place.DoesNotExist:
+        raise Http404("Place doesnt exists")
+
+    template = loader.get_template("reservations/places_one.html")
+    return HttpResponse(template.render({ "place": place }, request))
+
+
+def places_list(request):
+    places = Place.objects.all()
+
+    template = loader.get_template("reservations/places_list.html")
+    return HttpResponse(template.render({ "places": places }, request))
