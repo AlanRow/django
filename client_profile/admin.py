e14b5e721b38b90e6da087137abe2e147aad85cb
a.rogatskin <a.rogatskin@sitesoft.ru>
2026-08-23 16:17:40 +0300

    register client model in admin

--- client_profile/admin.py
+++ client_profile/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import ClientUser
 
-admin.site.register(ClientUser)
+
+@admin.register(ClientUser)
+class ClientUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Client profile", {
+            "fields": ("age", "location", "phone", "balance"),
+        }),
+    )
