90231f6785555cf0f22e61f19940d2ff5a89dd92
AlanRow <alekroga@gmail.com>
2026-08-24 21:02:15 +0300

    Custom validation

--- reservations/forms.py
+++ reservations/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, ValidationError
 
 from .models import Owner
 
@@ -7,3 +7,11 @@ class CreateOwnerForm(ModelForm):
     class Meta:
         model = Owner
         fields = ("name", "email")
+        
+    def clean_name(self):
+        name = self.cleaned_data["name"].strip()
+        
+        if len(name) < 3:
+            raise ValidationError("Name is too short (must have at least 3 chars)")
+        
+        return name
